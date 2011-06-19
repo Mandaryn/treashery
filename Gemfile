d7ddc9235d8b2c8a487eab87d8fa4a52bef668df
Piotr Marciniak <mandaryyyn@gmail.com>
2011-06-19 12:28:48 +0200

    initial project setup

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc4'
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'haml-rails'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem "bson_ext"
+gem "mongoid", ">= 2.0.0.beta.19"
+
+gem 'jquery-rails'
+gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treashery::Application.config.secret_token = '0110379a3d1e51f80c86f4ab855d053fb83ec75ae7a667f94665b3eef3f4fff3faa80fdb9ab73c79e8f191d5747745b294bfd0f5d6202f15b661e6bfd893d82c'
