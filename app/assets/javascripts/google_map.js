aa17e6c1774af66e330e56604108d14123de67d2
Michal <siemienczuk.michal@gmail.com>
2011-07-19 09:53:22 +0200

    Spots from list are marked at google map.

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,11 +1,34 @@
 $(document).ready(function initialize() {
-    var latlng = new google.maps.LatLng(51.994649, 19.039774); 
+	var map;
+	showMap();
+	addAllMarkers();
+});
+
+function showMap() {
+	  var latlng = new google.maps.LatLng(51.994649, 19.039774); 
     var myOptions = {
       zoom: 6,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("google_map"),myOptions);
-		}
-);
-  
+  	map = new google.maps.Map(document.getElementById("google_map"),myOptions);
+}
+
+function addAllMarkers() {
+	$("div.spot").each(function(index, element) {
+		var lat = parseFloat($(element).find("div.latitude").text());
+		var lng = parseFloat($(element).find("div.longitude").text());
+		var name = $(element).find("a.fn.url").text();
+			addMarker(lat, lng, name);
+	});
+}
+
+function addMarker(lat, lng, name) {
+	var spot = new google.maps.LatLng(lat,lng);
+  var markerOptions = {
+    position: spot,
+    map: map,
+    title: name
+  }
+  var marker = new google.maps.Marker(markerOptions);
+}
