f2a305883f5dc884a8ef5440c0947705cc125067
Piotr Marciniak <mandaryyyn@gmail.com>
2011-07-28 12:18:51 +0200

    reformat hashes, reduce types array

--- app/models/locality.rb
+++ app/models/locality.rb
@@ -6,38 +6,16 @@ class Locality
   field :neLng, type: Float
   field :swLat, type: Float
   field :swLng, type: Float
-  
-  validates_presence_of :name, :type, :neLat, :neLng, :swLat, :swLng
-  validates_numericality_of :neLat, :swLat, :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90
-  validates_numericality_of :neLng, :swLng, :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180
-  
+
+  validates :name, :type, :neLat, :neLng, :swLat, :swLng, presence: true
+  validates :neLat, :swLat, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
+  validates :neLng, :swLng, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
+
   has_many :localities
   has_many :spots
   belongs_to :locality
-  
+
   accepts_nested_attributes_for :localities
 
-  TYPES = ['street_address',
-    'route',
-    'intersection',
-    'political',
-    'country',
-    'administrative_area_level_1',
-    'administrative_area_level_2',
-    'administrative_area_level_3',
-    'colloquial_area',
-    'locality',
-    'sublocality',
-    'neighborhood',
-    'premise',
-    'subpremise',
-    'postal_code',
-    'natural_feature',
-    'airport',
-    'park',
-    'point_of_interest',
-    'post_box',
-    'street_number',
-    'floor',
-    'room']
+  TYPES = %w(route intersection political country administrative_area_level_1 administrative_area_level_2 administrative_area_level_3 colloquial_area locality sublocality neighborhood postal_code natural_feature airport park point_of_interest)
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,19 +1,19 @@
 class Spot
   include Mongoid::Document
-  field :name, :type => String
-  field :lng, :type => Float
-  field :lat, :type => Float
-  field :location, :type => Array
+  field :name, type: String
+  field :lng, type: Float
+  field :lat, type: Float
+  field :location, type: Array
   index [[ :location, Mongo::GEO2D ]], min: -180, max: 180
-  field :description, :type => String
-  field :address, :type => String
-  
-  validates_presence_of :name, :lat, :lng
-  validates_numericality_of :lat, :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90
-  validates_numericality_of :lng, :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180
-  
+  field :description, type: String
+  field :address, type: String
+
+  validates :name, :lat, :lng, presence: true
+  validates :lat, numericality: {greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
+  validates :lng, numericality: {greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
+
   belongs_to :locality
-  
+
   before_save :generate_location
 
   scope :bounded, ->(swLng, swLat, neLng, neLat) {
