a5a4cf2d389bf824d1bd21cba856778c4fd95efb
Michal <siemienczuk.michal@gmail.com>
2011-07-29 13:20:49 +0200

    pages_controller_spec.rb corrected

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe PagesController do
-
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
   describe "GET 'faq'" do
     it "should be successful" do
       get 'faq'
