30996f6f19736d66c25058c1e6d86a49d5ce0b58
Piotr Marciniak <mandaryyyn@gmail.com>
2011-07-31 21:14:23 +0200

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Treashery::Application.routes.draw do
-  get "pages/faq"
   match "faq" => "pages#faq", :as => :faq
-
-  get "pages/about"
   match "about" => "pages#about", :as => :about
-
-  get "pages/contact"
   match "contact" => "pages#contact", :as => :contact
 
   resources :localities
